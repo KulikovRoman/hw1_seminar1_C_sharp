c3ebef77b55022965c8576bc744b4bff9dd838a7
Roman Kulikov <r0m4ik.k@mail.ru>
2023-02-20 00:48:57 +0300

    add task 4

---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4. Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.  
+//     5 -> 2, 4
+//     8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число > ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+if (userNumber < 0)
+{
+    userNumber = -userNumber;
+}
+
+int count = 1;
+
+while (count <= userNumber)
+{
+    if (count % 2 == 0)
+    System.Console.Write($"{count} ");
+    count = count + 1;
+}
